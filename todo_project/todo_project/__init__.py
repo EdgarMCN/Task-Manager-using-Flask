897580cb19e28616b127d96f1518e3e36f1c0aa1
Edgar <edgarufba@gmail.com>
2026-06-14 02:05:47 -0300

    Aplicação completa

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 bcrypt = Bcrypt(app)
+
+# Prometheus metric
+REQUEST_COUNT = Counter("request_count", "Total requests")
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 # Always put Routes at end
 from todo_project import routes
--- todo_project/todo_project/routes.py
+++ todo_project/todo_project/routes.py
@@ -1,15 +1,16 @@
 from flask import render_template, url_for, flash, redirect, request
 
-from todo_project import app, db, bcrypt
+from todo_project import app, db, bcrypt, REQUEST_COUNT
 
 # Import the forms
-from todo_project.forms import (LoginForm, RegistrationForm, UpdateUserInfoForm, 
-                                UpdateUserPassword, TaskForm, UpdateTaskForm)
+from todo_project.forms import (
+    LoginForm, RegistrationForm, UpdateUserInfoForm,
+    UpdateUserPassword, TaskForm, UpdateTaskForm
+)
 
 # Import the Models
 from todo_project.models import User, Task
 
-# Import 
 from flask_login import login_required, current_user, login_user, logout_user
 
 
@@ -17,9 +18,11 @@ from flask_login import login_required, current_user, login_user, logout_user
 def error_404(error):
     return (render_template('errors/404.html'), 404)
 
+
 @app.errorhandler(403)
 def error_403(error):
     return (render_template('errors/403.html'), 403)
+
 
 @app.errorhandler(500)
 def error_500(error):
@@ -29,42 +32,48 @@ def error_500(error):
 @app.route("/")
 @app.route("/about")
 def about():
+    REQUEST_COUNT.inc()
     return render_template('about.html', title='About')
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+    REQUEST_COUNT.inc()
+
     if current_user.is_authenticated:
         return redirect(url_for('all_tasks'))
 
     form = LoginForm()
-    # After you submit the form
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        # Check if the user exists and the password is valid
+
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            task_form = TaskForm()
             flash('Login Successfull', 'success')
             return redirect(url_for('all_tasks'))
         else:
             flash('Login Unsuccessful. Please check Username Or Password', 'danger')
-    
+
     return render_template('login.html', title='Login', form=form)
-    
+
 
 @app.route("/logout")
 def logout():
+    REQUEST_COUNT.inc()
     logout_user()
     return redirect(url_for('login'))
 
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
+    REQUEST_COUNT.inc()
+
     if current_user.is_authenticated:
         return redirect(url_for('all_tasks'))
 
     form = RegistrationForm()
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, password=hashed_password)
@@ -79,6 +88,8 @@ def register():
 @app.route("/all_tasks")
 @login_required
 def all_tasks():
+    REQUEST_COUNT.inc()
+
     tasks = User.query.filter_by(username=current_user.username).first().tasks
     return render_template('all_tasks.html', title='All Tasks', tasks=tasks)
 
@@ -86,21 +97,28 @@ def all_tasks():
 @app.route("/add_task", methods=['POST', 'GET'])
 @login_required
 def add_task():
+    REQUEST_COUNT.inc()
+
     form = TaskForm()
+
     if form.validate_on_submit():
         task = Task(content=form.task_name.data, author=current_user)
         db.session.add(task)
         db.session.commit()
         flash('Task Created', 'success')
         return redirect(url_for('add_task'))
+
     return render_template('add_task.html', form=form, title='Add Task')
 
 
 @app.route("/all_tasks/<int:task_id>/update_task", methods=['GET', 'POST'])
 @login_required
 def update_task(task_id):
+    REQUEST_COUNT.inc()
+
     task = Task.query.get_or_404(task_id)
     form = UpdateTaskForm()
+
     if form.validate_on_submit():
         if form.task_name.data != task.content:
             task.content = form.task_name.data
@@ -110,14 +128,18 @@ def update_task(task_id):
         else:
             flash('No Changes Made', 'warning')
             return redirect(url_for('all_tasks'))
+
     elif request.method == 'GET':
         form.task_name.data = task.content
+
     return render_template('add_task.html', title='Update Task', form=form)
 
 
 @app.route("/all_tasks/<int:task_id>/delete_task")
 @login_required
 def delete_task(task_id):
+    REQUEST_COUNT.inc()
+
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
@@ -128,15 +150,19 @@ def delete_task(task_id):
 @app.route("/account", methods=['POST', 'GET'])
 @login_required
 def account():
+    REQUEST_COUNT.inc()
+
     form = UpdateUserInfoForm()
+
     if form.validate_on_submit():
-        if form.username.data != current_user.username:  
+        if form.username.data != current_user.username:
             current_user.username = form.username.data
             db.session.commit()
             flash('Username Updated Successfully', 'success')
             return redirect(url_for('account'))
+
     elif request.method == 'GET':
-        form.username.data = current_user.username 
+        form.username.data = current_user.username
 
     return render_template('account.html', title='Account Settings', form=form)
 
@@ -144,15 +170,17 @@ def account():
 @app.route("/account/change_password", methods=['POST', 'GET'])
 @login_required
 def change_password():
+    REQUEST_COUNT.inc()
+
     form = UpdateUserPassword()
+
     if form.validate_on_submit():
         if bcrypt.check_password_hash(current_user.password, form.old_password.data):
             current_user.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
             db.session.commit()
             flash('Password Changed Successfully', 'success')
-            redirect(url_for('account'))
+            return redirect(url_for('account'))
         else:
-            flash('Please Enter Correct Password', 'danger') 
+            flash('Please Enter Correct Password', 'danger')
 
     return render_template('change_password.html', title='Change Password', form=form)
-
